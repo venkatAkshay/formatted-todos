ad8de06bce621648bd55533152771d44a43f7afc
venkatAkshay <113366014+venkatAkshay@users.noreply.github.com>
2022-10-30 12:46:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,22 +9,22 @@ const todoList = () => {
 
   const overdue = () => {
     // Write the date check condition here and return the array of overdue items accordingly.
-    return all.filter((item) => item.dueDate < new Date().toLocalDateString("en-CA"));
+    return all.filter((item) => item.dueDate < new Date().toLocaleDateString("en-CA"));
   }
 
   const dueToday = () => {
     // Write the date check condition here and return the array of todo items that are due today accordingly.
-    return all.filter((item) => item.dueDate === new Date().toLocalDateString("en-CA"));
+    return all.filter((item) => item.dueDate === new Date().toLocaleDateString("en-CA"));
   }
 
   const dueLater = () => {
     // Write the date check condition here and return the array of todo items that are due later accordingly.
-    return all.filter((item) => item.dueDate > new Date().toLocatDateString("en-CA"));
+    return all.filter((item) => item.dueDate > new Date().toLocaleDateString("en-CA"));
   }
 
   const toDisplayableList = (list) => {
     // Format the To-Do list here, and return the output string as per the format given above.
-    return(list.map(item => `${item.completed ? '[x]': '[ ]'} ${item.title} ${item.dueDate === new Date().toLocalDateString("en-CA")}`));
+    return(list.map(item => `${item.completed ? '[x]': '[ ]'} ${item.title} ${item.dueDate === new Date().toLocaleDateString("en-CA")}`));
     
   }
 
